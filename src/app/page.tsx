c1179941377a2f40e156498cc82567dd11ef8c02
Alina Kozlyuk <talina787@icloud.com>
2026-08-10 14:07:49 +0300

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import About from "@/components/About/About";
+import Creators from "@/components/Creators/Creators";
+import Hero from "@/components/Hero/Hero";
+import PopularArticles from "@/components/PopularArticles/PopularArticles";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <main className="container">
+      <Hero />
+      <About />
+      <PopularArticles />
+      <Creators />
+    </main>
+  );
 }
